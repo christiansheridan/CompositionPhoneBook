0cee78d7b68496d88357ddc4e4f5fd365f36efe3
Christian Sheridan <christiansheridan@zipcoders-MacBook-Pro-13.local>
2018-10-28 20:55:59 -0400

    almost complete

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,65 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.TreeMap;
+import java.util.*;
+
+import static javax.swing.UIManager.put;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    String name;
+    ArrayList<String> phoneNumber = new ArrayList<>();
+    TreeMap<String, ArrayList<String>> phoneBook = new TreeMap<>();
+
+    public PhoneBook() {
+    }
+
+    public boolean hasEntry(String name) {
+        if (phoneBook.containsKey(name)){
+            return true;
+        } else {return false;}
+    }
+
+    public void add(String name, ArrayList<String> phoneNumber) {
+        phoneBook.put(name, phoneNumber);
+    }
+
+    public int size() {
+        return phoneBook.size();
+    }
+
+    public String lookUp(String name) {
+        if (phoneBook.containsKey(name)){
+            return name + "  " + phoneBook.get(name);
+        } else {return "name not found";}
+    }
+
+    public void remove(String name) {
+        if (phoneBook.containsKey(name)){
+            phoneBook.remove(name);
+        }
+    }
+
+    //convert to a set
+    public String reverseLookup(ArrayList<String> phoneNumber) {
+        String revLookupName = "";
+            for (Map.Entry<String, ArrayList<String>> map : phoneBook.entrySet()) {
+                if (map.getValue().equals(phoneNumber)) {
+                    revLookupName = map.getKey();
+                }
+            }
+         return revLookupName;
+    }
+
+    public String listNamesAndNumbers() {
+        String namesAndNumbers = "";
+        for (Map.Entry<String, ArrayList<String>> map : phoneBook.entrySet()){
+            namesAndNumbers += "Name: " + map.getKey() + "|   Phone Number(s): " + map.getValue();
+        }
+        System.out.println(namesAndNumbers);
+        return namesAndNumbers;
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,80 @@
 package com.zipcodewilmington.phonebook;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+    public PhoneBook book;
+
+    @Before
+    public void setup(){
+        book = new PhoneBook();
+        String name = "AA";
+        ArrayList <String> phoneNumber = new ArrayList<>();
+        phoneNumber.add("211-111-2222");
+        book.add(name, phoneNumber);
+    }
+
+    @Test
+    public void testHasEntry() {
+        //When
+        boolean expectedOutput = book.hasEntry("AA");
+        //Then
+        Assert.assertTrue(expectedOutput);
+    }
+
+    @Test
+    public void testAdd() {
+        //Then
+        int expectedOutput = 1;
+        Assert.assertEquals(expectedOutput, book.size());
+    }
+
+    @Test
+    public void testLookup(){
+        //When
+        String expectedOutput = "AA  [211-111-2222]";
+        String actualOutput = book.lookUp("AA");
+
+        //Then
+        Assert.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testRemove(){
+        //When
+        book.remove("AA");
+
+        //Then
+        Assert.assertEquals(0, book.size());
+    }
+
+    @Test
+    public void testReverseLookup(){
+
+        //When
+        String expectedOutput = "AA";
+        String actualOutput = book.reverseLookup(book.phoneNumber);
+
+        //Then
+        Assert.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testListNamesAndNumbers(){
+
+        //When
+        String expectedOutput = "AA 211-111-2222";
+        String actualOutput = book.listNamesAndNumbers();
+
+        System.out.println(book.listNamesAndNumbers());
+        //Then
+        //Assert.assertTrue(expectedOutput, actualOutput);
+    }
 }
